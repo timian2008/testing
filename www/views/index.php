6b510c17a0b53814f074f5b7313bcc870442aeca
timian2008 <timian2008@yandex.ru>
2015-12-06 21:53:49 +0400

    MVC

--- www/views/index.php
+++ www/views/index.php
@@ -6,6 +6,6 @@
  * Time: 19:41
  */
 foreach ($items as $item):?>
-    <h1><?php $item->id;?></h1>
-    <div><?php $item->name;?></div>
+    <h1><?php echo $item->id;?></h1>
+    <div><?php echo $item->name;?></div>
 <?php endforeach;?>
