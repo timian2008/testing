81a14bec8f08a0cd768e5302cd3c0348d62c0814
timian2008 <timian2008@yandex.ru>
2015-12-08 19:05:41 +0400

    MVC controller object

---
+++ www/controllers/NewsController.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__.'/../models/news.php';
+
+class NewsController
+{
+    public function actionAll(){
+        $items = News::getAll();
+        include __DIR__.'/../views/news/all.php';
+    }
+    public function actionOne(){
+        echo 'actionOne';
+        die;
+        $id= $_GET['id'];
+        $items = News::getOne();
+        include __DIR__.'/../views/news/one.php';
+    }
+}
