d951931296d83d89f8ae4ba2bef1eb20980e2b41
timian2008 <timian2008@yandex.ru>
2015-12-09 18:42:27 +0400

    full upgrade

---
+++ www/views/news/one.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mikhail
+ * Date: 09.12.2015
+ * Time: 16:40
+ */
+echo $item -> id;
+echo $item -> deptid;
+echo $item -> name;
+var_dump($item);
+//. $item -> deptid. $item -> name;
